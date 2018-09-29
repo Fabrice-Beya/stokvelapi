f660299686c04ac6374093b445f2b28bf5a0ed17
Fabrice Beya <byxfab001@myuct.ac.za>
2018-09-29 13:33:16 +0200

    switch to heroku

--- index.js
+++ index.js
@@ -5,22 +5,22 @@ var express             = require("express"),
     costumerRoutes      = require("./routes/costumer"),
     port                = process.env.PORT || 3001
 
-    
+
     // mongoose.connect('mongodb://localhost/stok');
-    // mongoose.connect("mongodb://stokvel-api.documents.azure.com:10255/?ssl=true&replicaSet=globaldb",{
-    //     auth:{  
-    //         user:"stokvel-api",
-    //         password:"gv2LsDnmozDRNlYNAfxKFbZP8wD4RO6EDKOvSTLsFtERBr0Vokq6uAbGcLI7ELIAzqVHQkkj0y8cL1cctRQAcw==",
-    //     }}, function(err, db){
+    mongoose.connect("mongodb://stokvel-api.documents.azure.com:10255/?ssl=true&replicaSet=globaldb",{
+        auth:{  
+            user:"stokvel-api",
+            password:"gv2LsDnmozDRNlYNAfxKFbZP8wD4RO6EDKOvSTLsFtERBr0Vokq6uAbGcLI7ELIAzqVHQkkj0y8cL1cctRQAcw==",
+        }}, function(err, db){
             
-    //     });
+        });
 
     app.use(bodyParser.json());
 
     app.get("/", function(req, res){
         res.send("Welcome to stokvel api");
     })
-    // app.use("/api/costumer", costumerRoutes);
+    app.use("/api/costumer", costumerRoutes);
 
     app.listen(port, function(){
         console.log("api server is live")
